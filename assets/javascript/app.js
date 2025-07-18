c7b88e971a807e173f881ea83ec90f603cb1b83b
Gustavo Ávila <71139006+gusavila@users.noreply.github.com>
2025-07-18 19:51:57 -0300

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+const searchBtn = document.querySelector(".search-btn");
+const searchBox = document.querySelector(".search-box");
+const searchBar = document.querySelector(".search-bar");
+
+let isExpanded = false;
+
+searchBtn.addEventListener("click", () => {
+  if (!isExpanded) {
+    searchBox.style.width = "160px";
+    searchBar.style.width = "100%";
+    searchBar.style.opacity = "1";
+    searchBar.style.padding = "4px 8px";
+    searchBar.focus();
+    isExpanded = true;
+  } else {
+    searchBox.style.width = "30px";
+    searchBar.style.width = "30px";
+    searchBar.style.opacity = "0";
+    searchBar.style.padding = "4px 0";
+    isExpanded = false;
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInside = searchBox.contains(e.target);
+
+  if (!isClickInside && isExpanded) {
+    searchBox.style.width = "30px";
+    searchBar.style.width = "30px";
+    searchBar.style.opacity = "0";
+    searchBar.style.padding = "4px 0";
+    isExpanded = false;
+  }
+});
